aab365691569804019a72d93f2a5325982b033ed
Flunder620 <jguzman12310@yahoo.com>
2023-12-07 23:59:34 -0600

    added components and some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import { Outlet } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer  />
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,2 +1,14 @@
 import './Navbar.scss'
+import { Link } from 'react-router-dom'
 
+export default function Navbar () {
+    return (
+        <nav className='navbar'>
+            <Link to ='/'>Home</Link>
+
+            <Link to ='/Projects'>Projects</Link>
+
+            <Link to ='/Info'>Info</Link>
+        </nav>
+    )
+}
