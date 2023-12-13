66b944f30573561734ebe39ee141d51ad9cf30b3
Flunder620 <jguzman12310@yahoo.com>
2023-12-12 21:53:37 -0600

    finished footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,28 @@
+import "./Footer.scss";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaStackOverflow } from "react-icons/fa";
+
+
 export default function Footer() {
-    return (
-        <footer className="footer">Footer</footer>
-    )
+  return (
+    <>
+      <footer className="footer">
+        <div class="footer-links">
+          <ul>
+            <li>
+              <a href="https://github.com/Flunder620" target="_blank"><FaGithub />Github</a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/jose-g-b6587a142/" target="_blank"><FaLinkedin />
+LinkedIn</a>
+            </li>
+            <li>
+              <a href="https://stackoverflow.com/users/21830099/flunder620" target="_blank"><FaStackOverflow />Stack Overflow</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  );
 }
