b90e010d273ac04855dde4f260060e69b63b36bd
Flunder620 <jguzman12310@yahoo.com>
2023-12-12 20:22:48 -0600

    began strucuter of contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,27 @@
+import './css/Contact.scss'
+import { useState, useEffect } from "react";
+
+
 export default function Contact() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(inputValue)
+    }
+
+
     return(
-        <h1>Contact</h1>
+        <>
+        <h1>Contact Me</h1>
+        <div>
+            <form className="contact-form">
+                <input type ='text' placeholder="Enter your name" required value ={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+                <input type ='text' placeholder="Enter your email" required value ={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+                <textarea placeholder="Send me a message!"></textarea>
+                <button onClick={handleSubmit}>Submit</button>
+            </form>
+        </div>
+        </>
     )
 }
