fecfacb7f1820813b602fdf9fc14e8197319512e
Flunder620 <jguzman12310@yahoo.com>
2023-12-14 22:08:06 -0600

    added link to resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
         <>
         <h1>Resume</h1>
-        <a href='' >Link to my resume</a>
+        <a href='https://www.dropbox.com/scl/fi/fxywheyd4ccd2jlm4qcav/Sample-Resume.docx?rlkey=zicqe1p4zxgr6bh3qp1t3ef2i&dl=0' >Link to my resume</a>
         <h2>Proficiencies: </h2>
         <ul>
             <li>
